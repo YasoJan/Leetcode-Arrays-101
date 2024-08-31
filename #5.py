02826482535a9bb1a1ca5ae42458e9582a1b9057
YasoJan <76984494+YasoJan@users.noreply.github.com>
2024-08-31 14:10:43 -0700

    Updated with two pointer approach bringing time complexity down from O(m*n) ---> O(m+n)

--- #5.py
+++ #5.py
@@ -1,91 +1,40 @@
-"""You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
-
-Merge nums1 and nums2 into a single array sorted in non-decreasing order.
-
-The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
-
-""" 
-
-"""
-Output: [1,2,2,3,5,6]
-Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
-The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
-Example 2:
-
-Input: nums1 = [1], m = 1, nums2 = [], n = 0
-Output: [1]
-Explanation: The arrays we are merging are [1] and [].
-The result of the merge is [1].
-Example 3:
-
-Input: nums1 = [0], m = 0, nums2 = [1], n = 1
-Output: [1]
-Explanation: The arrays we are merging are [] and [1].
-The result of the merge is [1].
-Note that because m = 0, there are no elements in nums1. The 0 is only there
-"""
-
 # merge nums1 & nums2 by overwritting nums1 no return value
 # only m elements in nums1 should be kept, and n should be ignored in nums1
 # only n elements in nums2 exist and should be merged 
 
 # Algorithm: Two Pointer Approach
-# edge cases for 0 and single element m & n
-    # 1. 
-    # nums1 = [0] m = 0
-    # nums2 = [1] n = 1
-        # if m == 0 and n == 1:
-            # nums1[0] = nums2[n] 
     
-    # 2.
-    # nums1 = [1] m = 1
-    # nums2 = [0] n = 0
-        # if m == 1 and n == 0:
-            # Do nothing 
-    
-    # 3. 
-    # nums1 = [0] m = 0
-    # nums2 = [0] m = 0
-        # if m == 0 and n == 0:
-            # Do nothing
-        
-# ensure m and n >= 0
-    # compare nums1[m] and nums2[n]
-        # Larger element takes nums1[length]
-            # if larger element is nums2 no switch required
-            # if larger element is nums1, switch nums1[m] and nums1[n]
-
-
-# nums1 [1,2,3,0,0,0]
-# nums2 [0,0,0,2,5,6]
-# result[1,2,3,0,0,6]
-# result[1,2,3,0,5,6]
-# result[1,2,2,3,5,6]
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         # Algorithm: Two Pointer Approach
-# edge cases for 0 and single element m & n
-    # 1. 
-    # nums1 = [0] m = 0
-    # nums2 = [1] n = 1
-        # if m == 0 and n == 1:
-            # nums1[0] = nums2[n] 
-    if m == 0 and n == 1:
-        nums1[0] == nums2[n]
-    
-    # 2.
-    # nums1 = [1] m = 1
-    # nums2 = [0] n = 0
-        # if m == 1 and n == 0:
-            # Do nothing 
-    
-    # 3. 
-    # nums1 = [0] m = 0
-    # nums2 = [0] m = 0
-        # if m == 0 and n == 0:
-            # Do nothing
-           
+        
+        # work backwards from the end of each list
+        # store largest value in the end of nums1
+            # working forward and shifting elements is too expensive O(m*n) 
+                # working backwards is O(m + n) as we are accessing each list element once from both 
+            # we also risk overwriting elements
+            
+        left_ptr = m-1
+        right_ptr = n-1
+        index = m+n-1
+            
+        while left_ptr >= 0 and right_ptr >= 0:
+            if nums1[left_ptr] > nums2[right_ptr]:
+                nums1[index] = nums1[left_ptr]
+                left_ptr-=1
+            else:
+                nums1[index] = nums2[right_ptr]
+                right_ptr-=1
+            index-=1
+        
+        # Edge case: m < n
+            # Iterate through nums1 and insert the remaining elements with nums2
+        while right_ptr >= 0:
+            nums1[index] = nums2[right_ptr]
+            left_ptr-=1
+            right_ptr-=1
+            index-=1
     
         
         
